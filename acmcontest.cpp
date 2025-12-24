3d3d626f92e2b433175a87997809622a5ae2fdc6
ZeyadGouda <zm41204@gmail.com>
2025-12-24 15:42:39 +0200

    test

--- acmcontest.cpp
+++ acmcontest.cpp
@@ -1,116 +1,33 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int arr[(int)2e3 + 5][(int)2e3 + 5];
-int freq[(int)2e3 + 5][(int)2e3 + 5];
-int freq2[(int)2e3 + 5][(int)2e3 + 5];
-int suf[(int)2e3 + 5][(int)2e3 + 5];
-int suf2[(int)2e3 + 5][(int)2e3 + 5];
+void FIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
 
+using ll = long long;
+
+void testcases()
+{
+    int n;
+    cin >> n;
+
+    vector<vector<int>> grid(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> grid[i][j];
+}
 int main()
 {
-	int n; cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cin >> arr[i][j];
-			freq[i][arr[i][j] - 1]++;
-			freq2[arr[i][j] - 1][j]++;
-		}
-	}
-	
-	//horizontal
-	for (int i = 0; i < n; i++)
-	{
-		suf[i][n - 1] = freq[i][n - 1];
-
-		for (int j = n - 1; j >= 0; j--)
-		{
-			suf[i][j] = suf[i][j + 1] + freq[i][j];
-		}
-	}
-	//vertical
-	for (int j = 0; j < n; j++)
-	{
-		suf2[n - 1][j] = freq2[n - 1][j];
-		for (int i = n - 1; i >= 0; i--)
-		{
-			suf2[i][j] = suf2[i + 1][j] + freq2[i][j];
-		}
-	}
-	int q; cin >> q;
-	for (int t = 0; t < q; t++)
-	{
-		int x, y;
-		cin >> x >> y;
-		for (int j = n; j >= 0; j--)
-		{
-			if (suf[x - 1][j] >= x)
-			{
-				cout << j + 1 << ' ';
-				break;
-			}
-		}
-		for (int i = n; i >= 0; i--)
-		{
-			if (suf2[i][y - 1] == y)
-			{
-				cout << i + 1 << endl;
-				break;
-			}
-		}
-		cout << endl;
-	}
-	/*cout << endl;
-	cout << "freq\n";
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << freq[i][j] << ' ';
-		}
-		cout << endl;
-	}
-	cout << endl;
-	cout << "suf\n";
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << suf[i][j] << ' ';
-		}
-		cout << endl;
-	}
-	cout << endl;
-	cout << "freq2\n";
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << freq2[i][j] << ' ';
-		}
-		cout << endl;
-	}
-	cout << endl;
-	cout << "suf2\n";
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << suf2[i][j] << ' ';
-		}
-		cout << endl;
-	}*/
-	return 0;
+    FIO();
+    int tests;
+    cin >> tests;
+    while (tests--)
+        testcases();
+    return 0;
 }
-	
-
-
-
-
-
-
-
-
-
+// git config --global user.email "you@example.com"
+// git config --global user.name "Your Name"
