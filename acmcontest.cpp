d8969866e968c93be78912d4aaf544dce13900c2
ZeyadGouda <zm41204@gmail.com>
2026-01-22 17:41:04 +0200

    test

--- acmcontest.cpp
+++ acmcontest.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+using ll = long long;
+ll M = 1e9 + 7;
 void FIO()
 {
     ios_base::sync_with_stdio(false);
@@ -8,26 +9,87 @@ void FIO()
     cout.tie(NULL);
 }
 
-using ll = long long;
-
 void testcases()
 {
     int n;
-    cin >> n;
-
-    vector<vector<int>> grid(n, vector<int>(n));
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> grid[i][j];
+    int r1, p1, s1;
+    string play;
+    cin >> n >> r1 >> p1 >> s1 >> play;
+    ll points = 0;
+    string ans;
+    for (int i = 0; i < play.size(); i++)
+    {
+        if (play[i] == 'R')
+        {
+            if (p1 > 0)
+            {
+                ans += 'P';
+                points++;
+                p1--;
+            }
+            else if (r1 > 0)
+            {
+                r1--;
+                ans += 'R';
+            }
+            else if (s1 > 0)
+            {
+                s1--;
+                ans += 'S';
+            }
+        }
+        if (play[i] == 'P')
+        {
+            if (s1 > 0)
+            {
+                points++;
+                s1--;
+                ans += 'S';
+            }
+            else if (p1 > 0)
+            {
+                ans += 'P';
+                p1--;
+            }
+            else if (r1 > 0)
+            {
+                r1--;
+                ans += 'R';
+            }
+        }
+        if (play[i] == 'S')
+        {
+            if (r1 > 0)
+            {
+                points++;
+                r1--;
+                ans += 'R';
+            }
+            else if (s1 > 0)
+            {
+                s1--;
+                ans += 'S';
+            }
+            else if (p1 > 0)
+            {
+                p1--;
+                ans += 'P';
+            }
+        }
+    }
+    if ((points >= (n + 1) / 2))
+        cout << "YES\n"
+             << ans << '\n';
+    else
+        cout << "NO\n";
 }
+
 int main()
 {
     FIO();
-    int tests;
+    int tests = 1;
     cin >> tests;
     while (tests--)
         testcases();
     return 0;
 }
-// git config --global user.email "you@example.com"
-// git config --global user.name "Your Name"
